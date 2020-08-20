a49288fb814810bca4de780caa55259a3a1e4256
Semih <semihozden01@gmail.com>
2020-08-20 21:47:25 +0300

    initialize project

---
+++ start/src/App.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react';
+import './App.css';
+
+function padTime(time){
+  return time.toString().padStart(2,'0');
+}
+
+export default function App() {
+  const [title,setTitle]=useState('Let the countdown begin!!!');
+  const [ timeLeft, setTimeLeft]=useState(25*60);
+
+  function startTimer(){
+    setInterval(()=>{
+      setTimeLeft(timeLeft=> {
+      
+      if(timeLeft>= 1) return timeLeft-1;
+      
+      return timeLeft-1
+      
+      }); 
+    },1000);
+  }
+
+  const minutes=padTime(Math.floor(timeLeft/60));
+  const seconds=padTime(timeLeft-minutes*60)
+
+  return (
+    <div className="app">
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        <button onClick={startTimer}>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+}
